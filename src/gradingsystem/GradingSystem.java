a2b8b191bb7add3918070c5427c961aab06d73e4
DinoMerx <DinoMerx@DESKTOP-HLDO6VV>
2021-03-09 18:54:01 +0800

    Print Test

--- src/gradingsystem/GradingSystem.java
+++ src/gradingsystem/GradingSystem.java
@@ -15,7 +15,7 @@ public class GradingSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello John");
     }
     
 }
